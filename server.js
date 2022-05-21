d4f005ab35ff135abcfe5dd82ef5d6197f8e9d15
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-05-20 20:40:37 -0400

    added movie API

--- server.js
+++ server.js
@@ -43,12 +43,12 @@ app.get('/weather', async (request, response, next) => {
   }
 });
 
-app.get('/.movie', async (request, response, next) => {
+app.get('/.movies', async (request, response, next) => {
   try {
     const searchQuery = request.searchQuery;
     const movieUrl = `https://api.themoviedb.org/3/movie?api_key=${proces.env.MOVIE_API_KEY}&query=${searchQuery}`;
     const movieResponse = await axios.get(movieUrl);
-    const movieResult = movieResponse.data.map(movie => new Movie(movie));
+    const movieResult = movieResponse.data.results.map(movie => new Movie(movie));
     response.status(200).send(movieResult);
   } catch (error) {
     error.customMessage = 'something went wrong on your API call.';
@@ -63,7 +63,15 @@ class Forecast {
   }
 }
 class Movie {
-
+  constructor(movie) {
+    this.title = movie.title;
+    this.description = movie.overview;
+    this.average_votes = movie.average_votes;
+    this.total_votes = movie.total_votes;
+    this.image_url = movie.image_url;
+    this.popularity = movie.popularity;
+    this.released_on = movie.released_on;
+  }
 }
 app.use((error, request, response, next) => {
   response.status(500).send(`Something went wrong on the server when making API call: ${error.customMessage} call the developer ${error.message} `);
