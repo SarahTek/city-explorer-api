2a48033d1e97a113b7f4f42496e3e1e7317e2235
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-05-20 20:18:39 -0400

    finished working with weather API

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
 'use strict';
 
+// this library lets us access our .env file
 require('dotenv').config();
+
+// express is a server library
 const express = require('express');
+
+// initalizes the express library
 const app = express();
+
+// library that determines who is allowed to speak to our server
 const cors = require('cors');
+// const { default: axios } = require('axios');
+// const { response } = require('express');
+const axios = require('axios');
+
+// this settting says that everyone is allowed to speak to our server
 app.use(cors());
+
+// we are getting the port variable from the .env file.
 const PORT = process.env.PORT;
+
+
+// this is a route. if you turn the server on and go to http://localhost:3001/ (or whatever port you specified in your .env), you will see 'hello from the home route'
 app.get('/', (request, response) => {
   response.send('Testing from home route');
 });
 
-app.get('/weather', (req, res, next) => {
+app.get('/weather', async (request, response, next) => {
   try {
-    const type = req.query.type;
-    console.log('type of list: ', type);
-    const forecastResult = new Forecast(type);
-    res.status(200).send(forecastResult);
+    const lat = request.query.lat;
+    const lon = request.query.lon;
+    console.log(lat, lon);
+    const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+    const weatherResponse = await axios.get(weatherUrl);
+    const weatherResults = weatherResponse.data.data.map(day => new Forecast(day));
+    response.status(200).send(weatherResults);
+  } catch (error) {
+    error.customMessage = 'something went wrong on your API call.';
+    console.error(error.customMessage + error);
+    next(error);
+  }
+});
+
+app.get('/.movie', async (request, response, next) => {
+  try {
+    const searchQuery = request.searchQuery;
+    const movieUrl = `https://api.themoviedb.org/3/movie?api_key=${proces.env.MOVIE_API_KEY}&query=${searchQuery}`;
+    const movieResponse = await axios.get(movieUrl);
+    const movieResult = movieResponse.data.map(movie => new Movie(movie));
+    response.status(200).send(movieResult);
   } catch (error) {
     error.customMessage = 'something went wrong on your API call.';
     console.error(error.customMessage + error);
@@ -23,19 +57,18 @@ app.get('/weather', (req, res, next) => {
   }
 });
 class Forecast {
-
-  static weather = require('./data/weather.json');
-  constructor(type) {
-    this.list = Forecast.weather.find(listObj => listObj.city_name === type);
-    this.arr = this.list.data.map(banana => ({
-      description: `${banana.low_temp} ${banana.max_temp} ${banana.weather.description}`,
-      date: `${banana.datetime}`
-    }));
-
+  constructor(day) {
+    this.description = `${day.low_temp} ${day.max_temp} ${day.weather.description}`;
+    this.date = `${day.datetime}`;
   }
+}
+class Movie {
+
 }
 app.use((error, request, response, next) => {
   response.status(500).send(`Something went wrong on the server when making API call: ${error.customMessage} call the developer ${error.message} `);
 })
 
+
+// this turns the server on to the port that you specifed in your .env file
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
