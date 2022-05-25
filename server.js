cb07d621d37618cafa1af051f5aef051229a1e4c
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-05-25 01:26:06 -0400

    updated server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const axios = require('axios');
 app.use(cors());
 
 // we are getting the port variable from the .env file.
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
 
 // this is a route. if you turn the server on and go to http://localhost:3001/ (or whatever port you specified in your .env), you will see 'hello from the home route'
@@ -27,10 +27,10 @@ app.get('/', (request, response) => {
   response.send('Testing from home route');
 });
 
-app.get('/weather', async (request, response, next) => {
+app.get('/weather', async (request, response) => {
   try {
-    const lat = request.query.lat;
-    const lon = request.query.lon;
+    const {lat , lon} = request.query;
+    // const lon = request.query.lon;
     console.log(lat, lon);
     const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
     const weatherResponse = await axios.get(weatherUrl);
@@ -43,39 +43,38 @@ app.get('/weather', async (request, response, next) => {
   }
 });
 
-app.get('/.movies', async (request, response, next) => {
+app.get('/movies', async (request, response) => {
   try {
-    const searchQuery = request.searchQuery;
-    const movieUrl = `https://api.themoviedb.org/3/movie?api_key=${proces.env.MOVIE_API_KEY}&query=${searchQuery}`;
+    const searchQuery = request.query.searchQuery.split(',')[0];
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
     const movieResponse = await axios.get(movieUrl);
     const movieResult = movieResponse.data.results.map(movie => new Movie(movie));
     response.status(200).send(movieResult);
   } catch (error) {
     error.customMessage = 'something went wrong on your API call.';
     console.error(error.customMessage + error);
-    next(error);
   }
 });
 class Forecast {
   constructor(day) {
-    this.description = `${day.low_temp} ${day.max_temp} ${day.weather.description}`;
-    this.date = `${day.datetime}`;
+    this.description = day.weather.description;
+    this.date = day.datetime;
   }
 }
 class Movie {
   constructor(movie) {
     this.title = movie.title;
     this.description = movie.overview;
-    this.average_votes = movie.average_votes;
-    this.total_votes = movie.total_votes;
-    this.image_url = movie.image_url;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = 'https://image.tmdb.org/t/p/w500'+ movie.poster_path;
     this.popularity = movie.popularity;
-    this.released_on = movie.released_on;
+    this.released_on = movie.release_date;
   }
 }
-app.use((error, request, response, next) => {
+app.use((error, request, response) => {
   response.status(500).send(`Something went wrong on the server when making API call: ${error.customMessage} call the developer ${error.message} `);
-})
+});
 
 
 // this turns the server on to the port that you specifed in your .env file
